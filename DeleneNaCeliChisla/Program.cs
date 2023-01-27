3f9cda07a84ee2bb1c5de4f753d166641be210a7
User <User@DESKTOP-7DEACCV>
2023-01-27 12:24:23 +0200

    Add project files.

---
+++ DeleneNaCeliChisla/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleneNaCeliChisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                sum += n1 % n2;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
